0ff17c3ee9270838c7891d37a850f3612b78b687
jennj <jennjenn93@gmail.com>
2022-05-05 18:15:41 -0500

    creating post routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,16 +10,16 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attribute: ['id','product_name', 'price', 'stock', 'category_id']
+        attribute: ['id', 'product_name', 'price', 'stock', 'category_id']
       }
     ]
   })
-  // be sure to include its associated Products
-  .then(dbCategoryData => res.json(dbCategoryData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    // be sure to include its associated Products
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -33,26 +33,36 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['product_name']
+        attributes: ['product_name', 'price', 'stock', 'category_id']
       }
     ]
   })
-  .then(dbCategoryData => {
-    if (!dbCategoryData) {
-      res.status(404).json({ message: 'No Category found with this id' });
-      return;
-    }
-    res.json(dbCategoryData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No Category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+    
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -63,14 +63,19 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+  
+    Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
