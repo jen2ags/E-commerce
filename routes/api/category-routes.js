15087160608790ae23d1fd39bdb546b739f4723a
jennj <jennjenn93@gmail.com>
2022-05-05 14:25:02 -0500

    creating sequelize connection in server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,6 +5,9 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    attributes: {}
+  })
   // be sure to include its associated Products
 });
 
