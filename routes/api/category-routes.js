d24f7b69b59e3835144428f5332d78867aa42e9a
jennj <jennjenn93@gmail.com>
2022-05-05 15:27:11 -0500

    adding get all products, categories, and tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attribute: ['product_name']
+        attribute: ['id','product_name', 'price', 'stock', 'category_id']
       }
     ]
   })
