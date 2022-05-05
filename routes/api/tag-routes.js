e9eda16050d723385c81a1c00dfe4ed63db0d00e
jennj <jennjenn93@gmail.com>
2022-05-05 18:00:54 -0500

    adding find one in all 3 models

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,15 +11,15 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id', 'product_tag']
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
       }
     ]
   })
-  .then(dbTagData => res.json(dbTagData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -33,10 +33,21 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: []
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
       }
     ]
   })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No Tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
